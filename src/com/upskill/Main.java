c83f0ec119afabedafac57f26e92d149c997a611
MarianaFMota <marianafmota96@gmail.com>
2022-05-10 15:25:05 +0100

    Alter Main Class

--- src/com/upskill/Main.java
+++ src/com/upskill/Main.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String expression;
+        String expression = null;
 
-        System.out.println("RPN Calculator");
-        System.out.println("Type the expression:");
+        System.out.println("---RPN Calculator---\n");
+        System.out.println("Type 'Exit' at any time to quit.\n");
+
 
         Scanner read = new Scanner(System.in);
-        expression = read.nextLine();
-        System.out.println(RPN.evaluate(expression));
+
+        while(expression != "Exit") {
+            System.out.println("Type the expression:");
+            expression = read.nextLine();
+            try{
+                System.out.println(RPN.evaluate(expression));
+            } catch(Exception e)
+            {
+                System.out.println("Invalid Expression. Try again or type 'Exit' to quit.");
+            }
+        }
     }
 }
