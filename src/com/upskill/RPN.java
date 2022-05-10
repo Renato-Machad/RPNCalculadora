129d6efb3f4a70fca4f35d27f78c6ac491790084
Sara Domingues <saraicdomingues@gmail.com>
2022-05-10 12:26:58 +0100

    FIX operation rpn

--- src/com/upskill/RPN.java
+++ src/com/upskill/RPN.java
@@ -1,10 +1,18 @@
 package com.upskill;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class RPN {
 
-    public static void operation(char operand,double a, double b){
-        Stack stack = new stack;
+    public static int operation(char operand,int a, int b){
+        HashMap<Character,Integer> operation = new HashMap<Character,Integer>();
+        operation.put('+',a + b);
+        operation.put('-',a - b);
+        operation.put('*',a * b);
+        operation.put('/',a / b);
 
+        return operation.get(operand);
     }
 
 }
