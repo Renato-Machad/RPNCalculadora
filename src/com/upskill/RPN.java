4751cd33511bd3d61abe09b7005e01ea1a492339
Sara Domingues <saraicdomingues@gmail.com>
2022-05-10 12:49:51 +0100

    CHANGE opertation method to switch

--- src/com/upskill/RPN.java
+++ src/com/upskill/RPN.java
@@ -6,18 +6,38 @@ import java.util.Hashtable;
 public class RPN {
 
     public static double evaluate(String expr) {
-               return 0;
+        return 0;
     }
 
 
-    public static int operation(char operand,int a, int b){
-        HashMap<Character,Integer> operation = new HashMap<Character,Integer>();
+    public static int operation(char operand, int a, int b) {
+
+        int x = 0;
+
+        switch (operand) {
+            case '+':
+                x = a + b;
+                break;
+            case '-':
+                x = a - b;
+                break;
+            case '*':
+                x = a * b;
+                break;
+            case '/':
+                x = a / b;
+                break;
+        }
+
+        return x;
+       /* HashMap<Character,Integer> operation = new HashMap<Character,Integer>();
         operation.put('+',a + b);
         operation.put('-',a - b);
         operation.put('*',a * b);
         operation.put('/',a / b);
 
         return operation.get(operand);
+               */
     }
 
 }
