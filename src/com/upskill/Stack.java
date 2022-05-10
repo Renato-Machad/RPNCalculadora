fddf7369a6bd047d14e1da40376fd8333cf9f57a
Renato Machado <renatomachado7214@gmail.com>
2022-05-10 12:24:32 +0100

    alteracao metodo push

--- src/com/upskill/Stack.java
+++ src/com/upskill/Stack.java
@@ -11,13 +11,13 @@ public class Stack {
     }
 
 
-    public void push(double data){
-        if(top == null)
+    public void push(int data){
+        if(top == null) // stack nao tem nada
             top = new Node(data);
         else
         {
-            Node current = new Node(data,top);
-            top = current;
+            Node numb = new Node(data,top);
+            top = numb;
         }
         size++;
     }
