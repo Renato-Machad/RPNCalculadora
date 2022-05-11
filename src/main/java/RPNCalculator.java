9e32aea8f920a68f621103846a517d486f1f660a
MarianaFMota <marianafmota96@gmail.com>
2022-05-11 11:50:15 +0100

    Update Main Class Menu and Correct RPN methods

--- src/main/java/RPNCalculator.java
+++ src/main/java/RPNCalculator.java
@@ -4,56 +4,58 @@ public class RPNCalculator {
 
     public static void main(String[] args) {
         Stack stack = new Stack();
-        String value = null;
+        RPN rpn = new RPN();
+        String value;
 
         System.out.println("---RPN Calculator---\n");
-        System.out.println("Type the expression:");
 
         Scanner read = new Scanner(System.in);
 
-        do {
-            value = read.nextLine();
-
-            stack.push(Integer.parseInt(value));
-
-            System.out.println("Do you want to add another value? (Yes-y/No-n?");
-            value = read.nextLine();
-        } while (value.equalsIgnoreCase("y"));
-
-        System.out.println(stack);
-        System.out.println(stack);
-
-        int option = 0;
+        int x = 0;
 
         do {
-            switch (option = RPNCalculator.reader()) {
+            switch (x = RPNCalculator.reader()) {
                 case 1:
-
+                    System.out.println("Type the expression:");
+                    value = read.nextLine();
+                    stack.push(Integer.parseInt(value));
                     break;
 
                 case 2:
-
+                    String result = String.valueOf(stack.pull());
+                    System.out.println("First element of the stack removed.");
                     break;
 
                 case 3:
-
+                    System.out.println(stack.toString());
                     break;
 
-                case 4:
+                case 10:
+                    rpn.sum();
+                    System.out.println((stack.getTop()).getData());
+                    break;
 
+                case 11:
+                    rpn.multiply();
+                    System.out.println((stack.getTop()).getData());
+                    break;
+
+                case 14:
+                    stack.convertIntoNegative((stack.getTop()).getData());
+                    System.out.println((stack.getTop()).getData());
                     break;
 
                 default:
                     System.out.println("Invalid expression.");
                     break;
             }
-        } while (value != "99");
+        } while (x != 99);
     }
 
     public static int reader() {
         Scanner read = new Scanner(System.in);
         int option = 0;
-        System.out.println("Choose from the following: \n" +
+        System.out.println("\nChoose from the following: \n" +
                 "1 - Add an element to the stack \n" +
                 "2 - Remove the first element from the stack \n" +
                 "3 - List the content of the stack \n" +
