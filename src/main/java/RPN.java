888f80e7f6f486efbe542371ed0f230e4d4dd138
MarianaFMota <marianafmota96@gmail.com>
2022-05-11 11:50:15 +0100

    Update Main Class Menu and Correct RPN methods

--- src/main/java/RPN.java
+++ src/main/java/RPN.java
@@ -5,81 +5,17 @@ public class RPN {
         stack = new Stack();
     }
 
-    public static int evaluate(String expr) {
-        int start = 0;
-        Stack stack = new Stack();
-
-        if (expr.isEmpty()) return 0;
-
-        do {
-            int space = expr.substring(start).indexOf(' ');
-            int end = space == -1 ? expr.length() : start + space;
-
-            String current = expr.substring(start, end);
-
-            if ("+-*/".indexOf(current.charAt(0)) != -1) {
-                int a = stack.pull();
-                int b = stack.pull();
-                stack.push(operation(current.charAt(0), b, a));
-            } else {
-                stack.push(Integer.parseInt(current));
-            }
-            start = end + 1;
-        } while (start < expr.length());
-
-        int result = stack.pull();
-
-        while (!stack.isEmpty()) {
-            int current = stack.pull();
-            result = current > result ? current : result;
-        }
-
-        return result;
-    }
-
-    public static int operation(char operand, int a, int b) {
-
-        int x = 0;
-
-        switch (operand) {
-            case '+':
-                x = a + b;
-                break;
-            case '-':
-                x = a - b;
-                break;
-            case '*':
-                x = a * b;
-                break;
-            case '/':
-                x = a / b;
-                break;
-        }
-
-        return x;
-       /* HashMap<Character,Integer> operation = new HashMap<Character,Integer>();
-        operation.put('+',a + b);
-        operation.put('-',a - b);
-        operation.put('*',a * b);
-        operation.put('/',a / b);
-
-        return operation.get(operand);
-               */
-    }
-
-    public static void sum() {
+    public void sum() {
         int a = stack.pull();
         int b = stack.pull();
         int result = a + b;
         stack.push(result);
     }
 
-
-    public static void multiply() {
+    public void multiply() {
         int a = stack.pull();
         int b = stack.pull();
-        int result = a + b;
+        int result = a * b;
         stack.push(result);
     }
-
 }
