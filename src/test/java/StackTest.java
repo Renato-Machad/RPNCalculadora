27b85235b4954995e0512dc7f651b971bab8acc0
Renato Machado <renatomachado7214@gmail.com>
2022-05-11 14:24:28 +0100

    test addValueToTopEmptyStack

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -5,20 +5,19 @@ import static org.junit.Assert.assertEquals;
 
 public class StackTest {
 
-//    @Test
-//    public void addValueToTopEmptyStack(){
-//        //arrange
-//        int num = 1;
-//        Node node = new Node(num);
-//        Stack stack = new Stack(node);
-//
-//        //Act
-//        Stack stackResult = new Stack();
-//        stackResult.push(num);
-//
-//        //assert
-//        assertEquals(stack,stackResult);
-//
-//    }
+    @Test
+    public void addValueToTopEmptyStack(){
+        //arrange
+        int num = 1;
+        Node node = new Node(num);
+        Stack stack = new Stack(node);
 
+        //Act
+        Stack stackResult = new Stack();
+        stackResult.push(stack.getTop().getData());
+
+        //assert
+        assertEquals(stack.getTop(),stackResult.getTop());
+
+    }
 }
