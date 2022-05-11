ee23643fce98bd00045576f66582a36631aa95e4
Sara Domingues <saraicdomingues@gmail.com>
2022-05-11 15:42:05 +0100

    Add listaStackSuccess

--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -1,3 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NodeTest {
-    
+    @Test
+    public void listStackSuccess() {
+        //Arrange
+        RPN rpn = new RPN();
+        Stack stack = rpn.getStack();
+        stack.push(7);
+        stack.push(3);
+        String expectedResult = "3  7  null";
+
+        //Act
+        String actualResult = stack.toString();
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+
+    public void listStackUncess(){
+        //Arrange
+        String expectedResult = "null";
+
+    }
+
 }
