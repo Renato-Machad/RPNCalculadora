984cbe99d9774609aa5659fecf1b6034895f8f57
Renato Machado <renatomachado7214@gmail.com>
2022-05-15 09:28:37 +0100

    Fix tests BinaryNumber class

--- src/test/java/BinaryNumberTest.java
+++ src/test/java/BinaryNumberTest.java
@@ -7,30 +7,28 @@ public class BinaryNumberTest {
     @Test
     public void integerToString() {
         //Arrange
-        int value = 5;
+        int value = 101;
         String valueString = String.valueOf(value); // "5"
-        String valueEmBinário = "101";
-        BinaryNumber binaryNumber = new BinaryNumber(valueString);
-        GeneralNumber binaryNumber2 = binaryNumber.integerToString(value);
 
         //Act
-        BinaryNumber binaryNumber3 = new BinaryNumber(valueString);
-        GeneralNumber binaryNumber1 = binaryNumber3.integerToString(value);
+        BinaryNumber binaryNumber = new BinaryNumber(valueString);
+        GeneralNumber result = binaryNumber.integerToString(5);
 
 
         //Assert
-        assertEquals(binaryNumber2, binaryNumber1);
+        assertEquals(new BinaryNumber(valueString), result);
     }
 
     @Test
     public void getValueDecimal() {
         //Arrange
-        GeneralNumber decimal = new DecimalNumber(5);
+        int number = 5;
 
         //Act
-        decimal.getValueDecimal(decimal);
+        DecimalNumber resultDecimal = new DecimalNumber(number);
+        int resultNumber = resultDecimal.getValueDecimal(resultDecimal);
 
         //Assert
-        assertEquals(decimal, decimal.getValueDecimal(decimal));
+        assertEquals(number, resultNumber);
     }
 }
