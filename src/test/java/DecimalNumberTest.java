c8d343bf33f5aae91829eb19b575e420bd1004aa
Renato Machado <renatomachado7214@gmail.com>
2022-05-18 21:02:44 +0100

    Add decimalToBinary test to DecimalNumberTest class to improve coverage

--- src/test/java/DecimalNumberTest.java
+++ src/test/java/DecimalNumberTest.java
@@ -1,8 +1,24 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class DecimalNumberTest {
+
+
+    @Test
+    public void decimalToBinary() {
+        //Arrange
+        int value = 5;
+        DecimalNumber decimalNumber = new DecimalNumber(value);
+        GeneralNumber expected = new DecimalNumber(value);
+
+        //Act
+        GeneralNumber result = decimalNumber.decimalToBinary(value);
+
+        //Assert
+        assertEquals(result, expected);
+    }
 
     @Test
     public void integerToDecimalSuccess() {
